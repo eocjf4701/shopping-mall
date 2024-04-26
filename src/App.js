f4046855d7f6363eca2a7ec5b7461f6b836c4d76
parkdaechul <eocjf4701@gmail.com>
2024-04-26 23:15:48 +0900

    로그인, 로그아웃 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import ProductAll from './page/ProductAll';
@@ -7,12 +8,16 @@ import Navbar from './component/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const [authenticate, setAuthenticate] = useState(false);
+  useEffect(() => {
+    console.log("authen -> ", authenticate);
+  }, [authenticate]);
   return (
     <div>
       <Navbar />
       <Routes>
         <Route path="/" element={<ProductAll />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/login" element={<Login setAuthenticate={setAuthenticate} />} />
         <Route path="/product/:id" element={<ProductDetail />} />
       </Routes>
     </div>
